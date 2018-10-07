06362da4fe5ed94160a0572c6ca035184a490e92
therealsqvad <nikita_klepov@mail.ru>
2018-10-07 23:31:00 +0700

    Исправил test в соответствии с eslint

--- 02/test.js
+++ 02/test.js
@@ -9,7 +9,7 @@ describe('Lesson 2', () => {
 
       timer(logger);
       setTimeout(() => {
-        expect(result).to.eql(new Array(10).fill(10).map((_, i)=> i));
+        expect(result).to.eql(new Array(10).fill(10).map(arrowFuncWithMaxNested()));
         done();
       }, 1000);
     });
@@ -26,7 +26,7 @@ describe('Lesson 2', () => {
         lastParams = params;
       }
 
-      let bindedFunc = customBind(call, context, 1, 3);
+      const bindedFunc = customBind(call, context, 1, 3);
 
       bindedFunc(4);
       expect(lastContext).to.eql(context);
@@ -43,7 +43,7 @@ describe('Lesson 2', () => {
         lastParams = params;
       }
 
-      let bindedFunc = customBind(call, context, 'hello', 'work');
+      const bindedFunc = customBind(call, context, 'hello', 'work');
 
       bindedFunc(777);
       expect(lastContext).to.be(context);
@@ -75,7 +75,7 @@ describe('Lesson 2', () => {
 
   describe('getIntersection', () => {
     it('should return sorted intersection of arrays', () => {
-      expect(getIntersection([1, 5, 7, 9, 3,], [1, 2, 3, 4])).to.eql([1, 3]);
+      expect(getIntersection([1, 5, 7, 9, 3], [1, 2, 3, 4])).to.eql([1, 3]);
       expect(getIntersection([1, 9, 10, 3, 5, 7], [10, 3, 4])).to.eql([3, 10]);
     });
   });
@@ -88,3 +88,7 @@ describe('Lesson 2', () => {
     });
   });
 });
+function arrowFuncWithMaxNested() {
+  return (_, i) => i;
+}
+
