822394b6dd48fb5fbed34cfc50da160001a16d01
therealsqvad <nikita_klepov@mail.ru>
2018-10-08 16:44:20 +0700

    Реализован первый тест на некорректную работу функции pickPeaks

--- additionalHomeWork02/tests.js
+++ additionalHomeWork02/tests.js
@@ -1,2 +1,29 @@
 // Type your tests here
 const expect = require('expect.js');
+const { pickPeaks, whoIsNext } = require('./functions');
+
+describe('Additional homework of Lesson 2', () => {
+  describe('pickPeaks', () => {
+    it('should find position and value of the "peaks" ', () => {
+      expect(
+        pickPeaks([1, 2, 2, 2, 1])
+      ).to.eql({
+        pos: [1],
+        peaks: [2]
+      });
+      expect(
+        pickPeaks([1, 2, 2, 2, 3])
+      ).to.eql({
+        pos: [],
+        peaks: []
+      });
+    });
+  });
+  describe('whoIsNext', () => {
+    it('should find who will drink n-th cola ', () => {
+      expect(
+        whoIsNext(['Sheldon', 'Leonard', 'Penny', 'Rajesh', 'Howard'], 1)
+      ).to.eql('Sheldon');
+    });
+  });
+});
