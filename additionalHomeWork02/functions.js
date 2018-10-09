9a91504d978327d486071d3e2c2f45ee2a9b2245
therealsqvad <nikita_klepov@mail.ru>
2018-10-09 16:23:21 +0700

    сделаны тесты для первой и второй задачи

--- additionalHomeWork02/functions.js
+++ additionalHomeWork02/functions.js
@@ -48,14 +48,6 @@ function pickPeaks(arr) {
   };
 }
 
-const array = [];
-
-for (let i = 0; i < 15; i++) {
-  array.push(Math.round(Math.random() * (4) + 1));
-}
-console.log(array);
-console.log(pickPeaks(array).pos, pickPeaks(array).peaks);
-
 // Sheldon, Leonard, Penny, Rajesh and Howard are in the queue for a "Double Cola" drink vending machine;
 // there are no other people in the queue. The first one in the queue (Sheldon) buys a can, drinks it and doubles!
 // The resulting two Sheldons go to the end of the queue.
--- additionalHomeWork02/tests.js
+++ additionalHomeWork02/tests.js
@@ -12,7 +12,13 @@ describe('Additional homework of Lesson 2', () => {
         peaks: [2]
       });
       expect(
-        pickPeaks([1, 2, 2, 2, 3])
+        pickPeaks([1, 3, 3, 3, 3, 2, 2, 2, 2, 3, 4, 2, 3, 2, 2])
+      ).to.eql({
+        pos: [1, 10, 12],
+        peaks: [3, 4, 3]
+      });
+      expect(
+        pickPeaks([1, 3, 3, 3, 4])
       ).to.eql({
         pos: [],
         peaks: []
@@ -24,6 +30,12 @@ describe('Additional homework of Lesson 2', () => {
       expect(
         whoIsNext(['Sheldon', 'Leonard', 'Penny', 'Rajesh', 'Howard'], 1)
       ).to.eql('Sheldon');
+      expect(
+        whoIsNext(['Sheldon', 'Leonard', 'Penny', 'Rajesh', 'Howard'], 52)
+      ).to.eql('Penny');
+      expect(
+        whoIsNext(['Sheldon', 'Leonard', 'Penny', 'Rajesh', 'Howard'], 7230702951)
+      ).to.eql('Leonard');
     });
   });
 });
